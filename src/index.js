f6bd68172dd32cb7f69dbd05cb9faf6c654bbe1a
Neto <netod90@gmail.com>
2023-02-07 12:09:35 -0700

    Add test basic structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './style.css';
+import './modules/comment_handler.js';
---
+++ src/modules/comment_handler.js
@@ -0,0 +1,4 @@
+import showData from './api.js';
+
+const commentButtons = document.querySelector('.comment');
+//  commentButtons.onClick = () => generateView();
