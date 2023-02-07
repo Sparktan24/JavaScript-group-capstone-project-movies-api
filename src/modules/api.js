8c8eff6ea458f275f642046312243f6553f71f86
Neto <netod90@gmail.com>
2023-02-07 15:34:21 -0700

    Add calls to API to get data

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,5 +1,5 @@
 const showData = async (query) => {
-  const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+  const url = `https://api.tvmaze.com/singlesearch/shows?q=${query}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
--- src/modules/comment_handler.js
+++ src/modules/comment_handler.js
@@ -1,4 +1,38 @@
 import showData from './api.js';
 
 const commentButtons = document.querySelector('.comment');
+const descriptionContent = document.querySelector('.description_content');
 //  commentButtons.onClick = () => generateView();
+
+const getDescription = async () => {
+  const data = await showData('1899');
+
+  const commentImage = document.querySelector('#comment-image');
+  commentImage.src = `${data.image.medium}`;
+  console.log('image link', data.image.medium);
+
+  const movieTitle = document.querySelector('.description_content__movie_title');
+  movieTitle.innerHTML = `Title: ${data.name}`;
+
+  const description = document.querySelector('#description');
+  description.innerHTML = data.summary;
+  //  descriptionContent.appendChild(description);
+
+  const ratingGenres = document.querySelector('.rating-genres');
+  ratingGenres.innerHTML = 'Genres: ';
+  data.genres.forEach((e, index, array) => {
+    const value = index !== array.length - 1 ? ratingGenres.innerHTML += `${e}, ` : ratingGenres.innerHTML += `${e}. `;
+
+    /*
+    if (index === array.length - 1) {
+      ratingGenres.innerHTML += `${e}. `;
+    } else {
+      ratingGenres.innerHTML += `${e}, `;
+    }
+    */
+  });
+  ratingGenres.innerHTML += `Rate: ${data.rating.average}`;
+  //  ratingGenres.appendChild(ratingGenres);
+};
+
+getDescription();
