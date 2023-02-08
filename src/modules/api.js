a94af701da37705228a0ce09cd0bfef399a1551e
ashmalzahra <ashmal.zahraa@gmail.com>
2023-02-09 01:33:45 +0500

    get Involvement API

--- src/modules/api.js
+++ src/modules/api.js
@@ -5,4 +5,26 @@ const showData = async (query) => {
   return data;
 };
 
-export default showData;
+const addLike = async (id) => {
+  const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/OX5sTwXK5eU2vy1wOpri/likes';
+  fetch(url,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: id,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+};
+
+const getLikes = async () => {
+  const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/OX5sTwXK5eU2vy1wOpri/likes';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+export {showData, addLike, getLikes};
+
