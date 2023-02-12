b471356ce83b6328388d88f0d110ff85bb0855e7
Neto <netod90@gmail.com>
2023-02-11 18:49:50 -0700

    Fix close button

--- src/modules/comments-button_handler.js
+++ src/modules/comments-button_handler.js
@@ -14,3 +14,13 @@ const commentsButtons = () => {
 }
 
 export default commentsButtons;
+
+
+//  CLOSE BUTTON POP-UP
+const btnClose = document.querySelector('#close-btn');
+const popUpContainer = document.querySelector('.comment_pop-up_container');
+
+btnClose.addEventListener('click', () => {
+  popUpContainer.classList.toggle('hidden');
+  document.querySelector('.comments-container').textContent = '';
+});
